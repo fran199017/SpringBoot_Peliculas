1f24ac08b2adf84b2770c86df931661c9f22e66d
Francisco Nicolau <79200586+fran199017@users.noreply.github.com>
2021-08-26 14:28:46 +0200

    Delete spring-boot-web directory

--- spring-boot-web/src/main/java/com/fran/app/controlador/entity/dao/IPeliculaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.fran.app.controlador.entity.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fran.app.controlador.entity.Pelicula;
-
-public interface IPeliculaDAO extends JpaRepository<Pelicula, Long>{
-
-}
